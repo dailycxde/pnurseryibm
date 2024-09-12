0293a0bb64c4d1a91aa4db73a8ad0471dc9cd835
HasanShkoukani <hasanshkoukani19@gmail.com>
2024-09-12 14:02:28 +0400

    msg

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../features/cart/cartSlice';
-import { save, load } from '../features/localstorage'
-
-const preloadedState = {
-  cart: load(),
-}
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
-  preloadedState
 });
-
-
-store.subscribe(() => {
-  const state = store.getState();
-  save(state.cart);
-})
 
 export default store;
